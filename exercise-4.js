c83554dfd2d24b602c8a0d016deef64a6d30365c
Simone Checco <simone.checco1995@gmail.com>
2023-01-14 16:16:19 +0100

    secondo esercizio completo

---
+++ exercise-4.js
@@ -0,0 +1,26 @@
+function nicknameMap(persons) {
+  let nickname;
+  let nick = [];
+  for (let i = 0; i < persons.length; i++) {
+    nickname = persons.at(i).name + "-" + persons.at(i).age;
+    nick.push(nickname);
+  }
+  return nick;
+}
+
+const persons = [
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
